ac65353646302c00c5ac53bbf94d93e19b9393d7
edoardopieropan <pieropan@hotmail.it>
2023-03-18 11:56:19 +0100

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'cama-website';
   currentLanguage: string = 'it';
+
+  ngOnInit(): void {
+    if(localStorage.getItem("language"))
+      this.currentLanguage = localStorage.getItem("language")!;
+  }
+
   changeLanguage(language: any){
+    localStorage.setItem("language", language);
     this.currentLanguage = language;
   }
 }
